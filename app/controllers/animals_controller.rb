1a82cb177e30dc42e8942069495e340e6bbbcd0f
kel-man <kyyafuso@email.com>
2021-08-12 12:57:00 -0700

    adds show and update functions and testing

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -5,8 +5,8 @@ class AnimalsController < ApplicationController
   end
 
   def show
-    animal = Animal.find(params[:id])
-    render json: animal
+    @animal = Animal.find(params[:id])
+    render('show')
   end
 
   def create
@@ -14,10 +14,21 @@ class AnimalsController < ApplicationController
   end
 
   def update
-
+    @animal = Animal.find(params[:id])
+    if @animal
+      @animal.update(animal_params)
+      render('show')
+    else
+      head 404
+    end
   end
 
   def destroy
 
+  end
+
+  private
+  def animal_params
+    params.require(:animal).permit(:common_name, :latin_name, :kingdom)
   end
 end
--- spec/requests/animals_spec.rb
+++ spec/requests/animals_spec.rb
@@ -1,7 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe "Animals", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:animal1){ Animal.create!({
+    common_name: 'mouse',
+    latin_name: 'mus musculus',
+    kingdom: 'mammal'
+  }) }
+  let(:animal2){ Animal.create!({
+    common_name: 'monitor lizard',
+    latin_name: 'varanus',
+    kingdom: 'reptile'
+  }) }
+  let(:animal3){ Animal.create!({
+    common_name: 'bald eagle',
+    latin_name: 'haliaeetus leucocephalus',
+    kingdom: 'raptor'
+  }) }
+
+  before do
+    animal1
+    animal2
+    animal3
+  end
+
+  describe "index" do
+    let(:request){ get '/animals' }
+    let(:expected_response) {
+      [{
+        "id" => animal1.id,
+        "common_name" => animal1.common_name,
+        "latin_name" => animal1.latin_name,
+        "kingdom" => animal1.kingdom,
+      }, {
+        "id" => animal2.id,
+        "common_name" => animal2.common_name,
+        "latin_name" => animal2.latin_name,
+        "kingdom" => animal2.kingdom,
+      }, {
+        "id" => animal3.id,
+        "common_name" => animal3.common_name,
+        "latin_name" => animal3.latin_name,
+        "kingdom" => animal3.kingdom,
+      }]
+    }
+
+    it 'returns a list of all the items in JSON' do
+      request
+      expect(JSON.parse(response.body)["animals"]).to include(*expected_response)
+    end
+  end
+
+  describe "show" do
+    let(:request){ get "/animals/#{animal2.id}" }
+    let(:expected_response) { {
+      id: animal2.id,
+      common_name: animal2.common_name,
+      latin_name: animal2.latin_name,
+      kingdom: animal2.kingdom,
+    }.to_json }
+    it 'shows only animal 2 from the database' do
+      request
+      expect(response.body).to eq expected_response
+    end
+  end
+
+  describe "create" do
+    let(:new_common){ "komodo dragon" }
+    let(:new_latin){ "varanus komodoensis" }
+    let(:request){ patch "/animals/#{animal2.id}", params: {
+      animal: {
+        common_name: new_common,
+        latin_name: new_latin,
+      }
+    } }
+    let(:request2){ get "/animals/#{animal2.id}" }
+    let(:expected_response) { {
+      "id" => animal2.id,
+      "common_name" => new_common,
+      "latin_name" => new_latin
+    } }
+    it 'edits the names of the monitor lizard to komodo dragon' do
+      request
+      request2
+      expect(JSON.parse(response.body)).to include expected_response
+      expect(animal2.reload.latin_name).to eq new_latin
+    end
   end
 end
